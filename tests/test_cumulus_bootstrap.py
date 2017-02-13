026a2e8e0cb99c3ad4ccd5798dc312d74aafba1e
Bobby Watson <bobby@apstra.com>
2017-02-13 10:15:42 -0500

    Added coveralls config

--- tests/test_cumulus_bootstrap.py
+++ tests/test_cumulus_bootstrap.py
@@ -1,4 +1,7 @@
+import sys
+import re
 from bin import cumulus_bootstrap
+from StringIO import StringIO
 
 
 def test_cli_parse():
@@ -24,3 +27,21 @@ def test_cli_parse():
                                          '--env_passwd', args['env_passwd']])
     for arg in vars(parse):
         assert str(getattr(parse, arg)) == args[arg]
+
+
+def test_setup_logging():
+
+    # Suppress stdout from log testing
+    old_stdout = sys.stdout
+    sys.stdout = StringIO()
+    try:
+        log = cumulus_bootstrap.setup_logging('test_log', None, '1.1.1.1')
+        log.info('testing logging')
+        sys.stdout.flush()
+        new_stdout = sys.stdout.getvalue()
+    finally:
+        # resume stdout
+        sys.stdout.close()
+        sys.stdout = old_stdout
+    log_search = re.search('(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2},\d{3}:INFO:1.1.1.1:testing logging\n)', new_stdout)
+    assert log_search.group(0) == new_stdout
